45d4b3a8f003a56cad3665190cfedf973380c172
gachiemchiep <gachiemchiep@gmail.com>
2016-11-14 23:50:12 +0900

    Add sample ovs

--- note/investigation/SDN_Software_Defined_Network/src/sample1/test_rule_default.py
+++ note/investigation/SDN_Software_Defined_Network/src/sample1/test_rule_default.py
@@ -7,5 +7,6 @@ for count in range(1, 5, 1):
     ipCount = count + 1
     if (count == 5):
         ipCount = 2
-    command = "Docker exec debian_%d -dit nmap -c1 --icmp  73.16.1.%d/24" % (count, ipCount)
+    command = "Docker exec debian_%d -dit nmap -c1 --icmp  173.16.1.%d/24" % (count, ipCount)
     print command
+    subprocess.call(command, shell=True)
