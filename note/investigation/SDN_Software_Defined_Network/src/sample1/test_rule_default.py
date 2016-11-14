8d7d1e651d0b9dc70c6214badaffa1a31cd7e99a
gachiemchiep <gachiemchiep@gmail.com>
2016-11-15 00:05:41 +0900

    Add sample ovs

--- note/investigation/SDN_Software_Defined_Network/src/sample1/test_rule_default.py
+++ note/investigation/SDN_Software_Defined_Network/src/sample1/test_rule_default.py
@@ -2,11 +2,14 @@
 
 import subprocess
 
-for count in range(1, 5, 1):
+for i in range(1, 10000):
 
-    ipCount = count + 1
-    if (count == 5):
-        ipCount = 2
-    command = "docker exec debian_%d nping -c1 --icmp  173.16.1.%d/24" % (count, ipCount)
-    print command
-    subprocess.call(command, shell=True)
+    for idCount in range(1, 6, 1):
+
+        for ipCount in range(2, 7, 1):
+
+            if not (idCount +1) == ipCount:
+                command = "docker exec debian_%d nping -c1 --icmp  173.16.1.%d/24" % (idCount, ipCount)
+            print command
+            subprocess.call(command, shell=True)
+
